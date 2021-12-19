b6bc0603118bc446e3194f1960e77eda2cab1a0a
guest1 <2003.07.o1@gmail.com>
2021-12-19 20:33:45 +0600

    interface

---
+++ app/src/main/java/kg/nurbolot/clever_coffe/BarKuhnyaActivity.kt
@@ -0,0 +1,11 @@
+package kg.nurbolot.clever_coffe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarKuhnyaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bar_kuhnya)
+    }
+}
---
+++ app/src/main/java/kg/nurbolot/clever_coffe/CoffeeForFreeActivity.kt
@@ -0,0 +1,11 @@
+package kg.nurbolot.clever_coffe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeForFreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee_for_free)
+    }
+}
